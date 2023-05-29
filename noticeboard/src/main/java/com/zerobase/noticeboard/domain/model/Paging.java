4eec659ab37e83e5de6854d96ddf535c53c44cbf
Choain <cangiecho@gmail.com>
2023-05-29 21:07:57 +0900

    회원가입 및 email TOKEN

---
+++ noticeboard/src/main/java/com/zerobase/noticeboard/domain/model/Paging.java
@@ -0,0 +1,64 @@
+package com.zerobase.noticeboard.domain.model;
+
+import com.zerobase.noticeboard.domain.post.LprodVO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.h2.mvstore.Page;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Paging {
+    private int pageSize;
+    private int totalPage;
+    private int pageNum;
+    private int pageNavSize;
+    private long totalRecode;
+    private long startRowIndex;
+    private int firstPage;
+    private int lastPage;
+
+    private boolean hasPreviousPage;
+    private boolean hasNextPage;
+    private boolean hasFirstPage;
+    private boolean hasLastPage;
+
+    public Paging(int pageNum, long totalRecode, int pageSize, int pageNavSize) {
+        this.pageNum = pageNum;
+        this.totalRecode = totalRecode;
+        this.pageSize = pageSize;
+        this.pageNavSize = pageNavSize;
+
+        if (pageNum < 1) {
+            pageNum = 1;
+        }
+        if (pageNavSize < 1) {
+            pageSize = 5;
+        }
+        calculation();
+    }
+
+    private void calculation() {
+        totalPage = (int) (((totalRecode - 1) / pageSize) + 1);
+        if (pageNum > totalPage) {
+            pageNum = totalPage;
+        }
+
+        firstPage = (((int) Math.ceil((float) pageNum / (float) pageNavSize)) - 1) * pageNavSize + 1;
+        lastPage = firstPage + pageNavSize - 1;
+        if (lastPage > totalPage) {
+            lastPage = totalPage;
+        }
+
+        startRowIndex = totalRecode - ((long) (pageNum - 1) * pageSize);
+
+        hasPreviousPage = firstPage != 1;
+        hasNextPage = ((long) lastPage * pageSize) < totalRecode;
+
+        hasFirstPage = (pageNum > 1 && pageNavSize < pageNum);
+        hasLastPage = (pageNum < totalPage && lastPage + 1 < totalPage);
+    }
+}
