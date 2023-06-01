8222ec806be2c5f25d451a9edc261a42d1ffc5d4
Choain <cangiecho@gmail.com>
2023-06-01 16:04:24 +0900

    게시판 프로그램

--- noticeboard/src/main/java/com/zerobase/noticeboard/service/PostListService.java
+++ noticeboard/src/main/java/com/zerobase/noticeboard/service/PostListService.java
@@ -4,6 +4,7 @@ import com.zerobase.noticeboard.domain.model.Post;
 import com.zerobase.noticeboard.domain.post.UpdatePostForm;
 import com.zerobase.noticeboard.domain.repository.PostRepository;
 import com.zerobase.noticeboard.exception.ErrorCode_b;
+import com.zerobase.noticeboard.exception.PostException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,7 +18,7 @@ public class PostListService {
     public Post updatePost(Long id, UpdatePostForm form) {
         Post post = postRepository.findById(form.getId())
                 .filter(pi -> pi.getId().equals(id))
-                .orElseThrow(() -> new MemberException(ErrorCode_b.NOT_FOUND_POST));
+                .orElseThrow(() -> new PostException(ErrorCode_b.NOT_FOUND_POST));
         return post;
     }
 
@@ -25,7 +26,7 @@ public class PostListService {
     public void deletePost(Long id) {
         Post post = postRepository.findById(id)
                 .filter(pi -> pi.getId().equals(id))
-                .orElseThrow(() -> new MemberException(ErrorCode_b.NOT_FOUND_POST));
+                .orElseThrow(() -> new PostException(ErrorCode_b.NOT_FOUND_POST));
         postRepository.delete(post);
     }
 }
